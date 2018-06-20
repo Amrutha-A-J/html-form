abbb44d032d72b4866266d2622a73a6626d2430a
Amrutha-A-J <amruthaajadiyath@gmail.com>
2018-06-20 17:10:34 +0530

    finished all features

--- script/script.js
+++ script/script.js
@@ -1,1 +1,38 @@
-var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+form_container = document.getElementById("form_container");
+data_container = document.getElementById("data_container");
+var existing_array = JSON.parse(localStorage.getItem('stored_array'));
+var obj_array = existing_array || [];
+
+function f_submit() {
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var country = document.getElementById('country').value;
+    var email = document.getElementById('email').value;
+    var psw = document.getElementById('psw').value;
+    var obj = {
+        fname: fname,
+        lname: lname,
+        country: country,
+        email: email,
+        psw: psw
+    }
+    obj_array.push(obj);
+    localStorage.setItem('stored_array', JSON.stringify(obj_array));
+}
+
+function f_retrieve() {
+    var data = JSON.parse(localStorage.getItem('stored_array'));
+    var tbody = document.getElementById("data_table");
+    data.forEach(element => {
+        var td = [];
+        var tr;
+        var key = Object.keys(element);
+        tr = document.createElement("tr");
+        key.forEach(prop => {
+            td = document.createElement("td");
+            td.appendChild(document.createTextNode(element[prop]));
+            tr.appendChild(td);
+        });
+        tbody.appendChild(tr);
+    });
+}
